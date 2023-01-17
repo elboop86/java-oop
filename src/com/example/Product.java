bb8c473207e2c9ceccebefe1c16f02b6660fc5f5
elboop86 <javi86medina@hotmail.com>
2023-01-17 11:21:21 +0100

    clase Producto

---
+++ src/com/example/Product.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Product {
+    // atributos
+    private String name;
+    private Double price;
+    private String color;
+    private Integer weight;
+
+    // constructores
+
+
+    // getter setter
+
+
+    //tostring
+
+}
